c5ff62164d8c11952299bdcfe73cd25d5c0c0e33
xuduo <xuduo96@outlook.com>
2019-05-11 16:54:46 +0800

    first commit

---
+++ SpringCloud/eureka-member/src/main/java/com/example/eurekaorder/EurekaOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekaorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaOrderApplication.class, args);
+    }
+
+}
